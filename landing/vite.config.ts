e77741ec362cca37ead49cfed5462998f486055b
Abdullah <48760103+abdebek@users.noreply.github.com>
2025-06-11 08:43:09 +0300

    Fix CORS issues for Module Federation

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -14,6 +14,14 @@ export default defineConfig({
       shared: ['react', 'react-dom']
     })
   ],
+  server: {
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  },
   build: {
     modulePreload: false,
     target: 'esnext',
